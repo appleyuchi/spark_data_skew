92932bc96c6fd07c21cdfd1415e25712eb975b61
appleyuchi <appleyuchi@tom.com>
2020-08-15 21:21:14 +0800

    完成scala自定义partition

--- Scala/parallel_config/src/main/scala/parallel_config.scala
+++ Scala/parallel_config/src/main/scala/parallel_config.scala
@@ -1,59 +1,44 @@
-import com.sun.rowset.internal.Row
-import org.apache.spark.api.java.JavaPairRDD
-import org.apache.spark.api.java.function._
-import scala.Tuple2
-import java.util._
-import java.util.Random
-import java.util
-import org.apache.spark.SparkConf
-import org.apache.spark.api.java.JavaSparkContext
-import java.lang._
+import org.apache.spark.{Partitioner, SparkConf, SparkContext}
 import org.apache.log4j.Logger
 
-import org.apache.spark.SparkConf
-import org.apache.spark.api.java.JavaPairRDD
-import org.apache.spark.api.java.JavaSparkContext
-import org.apache.spark.api.java.function.PairFunction
 import scala.Tuple2
 
-object parallel_config{
+object parallel_config {
   def main(args: Array[String]): Unit = { //消除warning
     Logger.getLogger("org.apache.hadoop").setLevel(org.apache.log4j.Level.WARN)
     Logger.getLogger("org.apache.spark").setLevel(org.apache.log4j.Level.WARN)
     Logger.getLogger("org.project-spark").setLevel(org.apache.log4j.Level.WARN)
-    val conf = new SparkConf().setMaster("spark://Desktop:7077").setJars(Array[String]("/home/appleyuchi/桌面/Spark数据倾斜处理/Scala/parallel_config/target/sampling-salting-1.0-SNAPSHOT.jar")).setAppName("join")
-    val sc = new JavaSparkContext(conf)
+    //    val conf = new SparkConf().setMaster("spark://Desktop:7077").setJars(Seq(" /home/appleyuchi/桌面/Spark数据倾斜处理/Scala/parallel_config/target/parallel_config-1.0-SNAPSHOT.jar")).setAppName("join").set("spark.cores.max","2").set("spark.driver.memory","512m")
+    //  .set("spark.executor.memory","512m")
+    val conf = new SparkConf().setMaster("spark://Desktop:7077").setJars(Array("/home/appleyuchi/桌面/Spark数据倾斜处理/Scala/parallel_config/target/parallel_config-1.0-SNAPSHOT.jar")).setAppName("join").set("spark.cores.max", "2").set("spark.driver.memory", "512m")
+    val sc = new SparkContext(conf)
+
     //----------------------------面是数据读取----------------------------
+    //    val path1 = "hdfs://Desktop:9000/rdd3.csv"
+    //    var rdd1 = sc.textFile(path1).map(line => line.split(",")).map { case Array(f1, f2) => (f1, f2) }
+
+
     val path1 = "hdfs://Desktop:9000/rdd1.csv"
     val path2 = "hdfs://Desktop:9000/rdd2.csv"
-    val rdd1 = sc.textFile(path1).mapToPair(new PairFunction[String, Long, String]() {
-      @throws[Exception]
-      override def call(s: String): Tuple2[Long, String] = {
-        val strings = s.split(",")
-        val ids = Long.valueOf(strings(0))
-        val greet = strings(1)
-        Tuple2.apply(ids, greet)
+
+
+    var rdd1 = sc.textFile(path1).map(line => line.split(",")).map { case Array(f1, f2) => (f1, f2) }
+
+    var rdd4=rdd1.groupByKey(new Partitioner {
+      override def numPartitions: Int =
+      {
+        return 12
+      }
+
+      override def getPartition(key: Any): Int = {
+        val id = key.toString.toInt
+        if (id >= 9500000 && id <= 9500084 && ((id - 9500000) % 12) == 0) (id - 9500000) / 12
+        else id % 12
       }
     })
-    val rdd2_list = rdd1.groupByKey(12,)
 
 
+    println("rdd4=\n",rdd4.collect())
 
-//    new ForeachPartitionFunction[] {}
-
-
-//      .groupByKey(new Nothing() {
-//      def numPartitions = 12
-//
-//      def getPartition(key: Any): Int
-//      =
-//      {
-//        val id = key.toString.toInt
-//        if (id >= 9500000 && id <= 9500084 && ((id - 9500000) % 12) == 0) (id - 9500000) / 12
-//        else id % 12
-//      }
-//    }).collect
-//    //        这里的new Partitioner也可以整体替换为一个具体的数字。
-    println(rdd2_list)
   }
 }
